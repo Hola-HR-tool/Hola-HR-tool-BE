250529a70976fb7dc2d1af0d0de06a038f6927e0
kim-sun-a <gina305496@gmail.com>
2023-03-06 20:59:50 +0900

    init

---
+++ src/main/java/com/hola/hr/tool/HolaHrToolBeApplication.java
@@ -0,0 +1,13 @@
+package com.hola.hr.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaHrToolBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolaHrToolBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hola/hr/tool/HolaHrToolBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hola.hr.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaHrToolBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
